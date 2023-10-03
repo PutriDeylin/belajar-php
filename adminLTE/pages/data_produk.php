fd4d9636961a1e247e6e9304bd98d02d386f5cb1
PutriDeylin <putrideylin82@gmail.com>
2023-10-03 09:31:24 +0700

    first commit

---
+++ adminLTE/pages/data_produk.php
@@ -0,0 +1,15 @@
+<?php
+$produkrow1 = array(
+    array("img" => "../dist/img/1.jpg", "title" => "Sepatu Kets Wanita Casual", "description" => "Ukuran 36, 37, 38, 39, 40", "price" => "Rp200.000"),
+    array("img" => "../dist/img/2.jpg", "title" => "Sepatu Kets Wanita Casual", "description" => "Ukuran 36, 37, 38, 39, 40", "price" => "Rp250.000"),
+    array("img" => "../dist/img/3.jpg", "title" => "Sepatu Kets Wanita Casual", "description" => "Ukuran 36, 37, 38, 39, 40", "price" => "Rp300.000"),
+    array("img" => "../dist/img/4.jpg", "title" => "Sepatu Kets Wanita Casual", "description" => "Ukuran 36, 37, 38, 39, 40", "price" => "Rp300.000"),
+);
+
+$produkrow2 = array(
+    array("img" => "../dist/img/5.jpg", "title" => "Sepatu Kets Wanita Casual", "description" => "Ukuran 36, 37, 38, 39, 40", "price" => "Rp500.000"),
+    array("img" => "../dist/img/6.jpg", "title" => "Sepatu Kets Wanita Casual", "description" => "Ukuran 36, 37, 38, 39, 40", "price" => "Rp150.000"),
+    array("img" => "../dist/img/7.jpg", "title" => "Sepatu Kets Wanita Casual", "description" => "Ukuran 36, 37, 38, 39, 40", "price" => "Rp170.000"),
+    array("img" => "../dist/img/8.jpg", "title" => "Sepatu Kets Wanita Casual", "description" => "Ukuran 36, 37, 38, 39, 40", "price" => "Rp180.000"),
+);
+?>
