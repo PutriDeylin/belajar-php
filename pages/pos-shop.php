d47cb0220b25af74d5461bae8246d990d93e47d8
PutriDeylin <putrideylin82@gmail.com>
2023-10-24 19:41:49 +0700

    [STYLE] ubah sedikit penulisan

--- pages/pos-shop.php
+++ pages/pos-shop.php
@@ -151,7 +151,7 @@
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1>Product Tugas 17, 18</h1>
+                            <h1>CRUD Products</h1>
                         </div>
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
